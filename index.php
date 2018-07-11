13b6fda897c9f39c4b370d2009abfd53d7cd813f
SyeedHasan <naqvi_hasan@live.com>
2018-07-11 10:23:39 +0500

    Index started
    Made file

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
-echo "Hi!";
-
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+<body>
+
+    <div class="nav">   
+        <p class="logo" >EdModo</p>
+
+    </div>
+
+
+
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
