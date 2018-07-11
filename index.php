36475d3dbf68e3dba02f3e4719690bcde754263f
AlishbaTasleem <41019089+AlishbaTasleem@users.noreply.github.com>
2018-07-11 23:15:38 +0500

    bootstrap updated2

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
             <li>Profile</li>
         </ul>
     </div> -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black">
   <a class="navbar-brand" href="#"> 
       <img src="assets/images/logos/pl.png" width="30" height="30" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
