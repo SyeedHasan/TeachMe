bd9d8e89c090087181564efa21d3465129c445a3
AlishbaTasleem <41019089+AlishbaTasleem@users.noreply.github.com>
2018-07-13 23:11:35 +0500

    settings page updated

--- settings.php
+++ settings.php
@@ -1,6 +1,14 @@
 <?php
     include 'includes/header.php';
-?>
+?><link rel="stylesheet" href="assets/css/settings.css">
+<div class="mainClass">
+    <ul class="contents">
+    <li class="link linkBorderA" onclick="location.href = 'http://stackoverflow.com/questions/3486110/make-a-list-item-clickable-html-css';"><a class="anchorTags" href="#">Personal Information</a></li>
+    <li class="link linkBorderA"><a class="anchorTags" href="#">Password</a></li>
+    <li class="link"><a class="anchorTags" href="#">Notification </a></li>
+</div>
+    
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
