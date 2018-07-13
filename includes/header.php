dcc53b6edacd7f0846d7d04db4b608dc006b85f7
SyeedHasan <naqvi_hasan@live.com>
2018-07-13 19:05:30 +0500

    completed header
    Don't revert atm. Check timestamp.

---
+++ includes/header.php
@@ -0,0 +1,103 @@
+<?php
+
+require 'config/config.php';
+
+if (isset($_SESSION['username'])) {
+    //If user is logged in, it contains the username
+    $userLoggedIn = $_SESSION['username'];
+    $firstName = $_SESSION['firstName'];
+    $lastName = $_SESSION['lastName'];
+
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+
+    $user = mysqli_fetch_array($user_details_query);
+    //Has an array of all user data.
+
+} else {
+    header("Location: login.php");
+}
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/images/icons/favicons/icons.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-xl navbar-dark bg-black">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/images/logos/pl.png" width="30" height="30" alt="">
+        </a>
+
+
+        <!-- These are the logos without the toggling effect applied to them -->
+        <div class="d-flex order-lg-1 ml-auto pr-2">
+            <ul class="navbar-nav flex-row">
+                <li class="nav-item mx-2 mx-lg-0">
+                    <a class="nav-link white" href="#"><?php echo $firstName . " " . $lastName; ?></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link white" href="includes/handlers/logout.php">Log Out</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Icons to be toggled -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-home"></i>Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-graduation-cap"></i>Classes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-book"></i>Assignments</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-list-ul"></i>To-Do</a>
+                </li> -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        <i class="fas fa-chevron-down"></i>More
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Y</a>
+                        <a class="dropdown-item" href="settings.php">Settings</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"><i class="fas fa-list-ul"></i>To-Do</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
