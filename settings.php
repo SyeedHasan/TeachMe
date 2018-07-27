4d0bb71bf68667df58c23e6e79a0fe2c92ba0b30
SyeedHasan <naqvi_hasan@live.com>
2018-07-27 18:07:36 +0500

    fixed right arrow

--- settings.php
+++ settings.php
@@ -1,21 +1,19 @@
 <?php
-    include 'includes/header.php';
-?>
 
+include("includes/header.php");
+
+?>
+    
 <link rel="stylesheet" href="assets/css/settings.css">
 
 <div class="mainClass">
     <ul class="contents">
     <li class="link linkBorderA"><p class="anchorTags">Account Settings</p></li>
-    <li class="link linkBorderA" ><a class="anchorTags" href="#">Personal Information</a><i class="fas fa-chevron-right"></i></li>
-    <li class="link linkBorderA"><a class="anchorTags" href="#">Password</a></li>
-    <li class="link"><a class="anchorTags" href="#">Notification </a></li>
+    <li class="link linkBorderA" ><a class="anchorTags" href="#">Personal Information<i class="fas fa-chevron-right"></i></a></li>
+    <li class="link linkBorderA"><a class="anchorTags" href="#">Password<i class="fas fa-chevron-right"></i></a></li>
+    <li class="link"><a class="anchorTags" href="#">Notification<i class="fas fa-chevron-right"></i></a></li>
 </div>
     
-
-
-
-
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
