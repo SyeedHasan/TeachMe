7daa9f5710e6348252b0c265cfb367ac53acd18f
AlishbaTasleem <41019089+AlishbaTasleem@users.noreply.github.com>
2018-07-29 16:56:19 +0500

    changed settings

--- settings.php
+++ settings.php
@@ -13,6 +13,36 @@ include("includes/header.php");
     <li class="link linkBorderA"><a class="anchorTags" href="#">Password<i class="fas fa-chevron-right"></i></a></li>
     <li class="link"><a class="anchorTags" href="#">Notification<i class="fas fa-chevron-right"></i></a></li>
 </div>
+<div class="sClass">
+<p id="p">Personal information <p>
+    <hr>
+<p id="a">Phone Number </p>
+<form id="a"action="#" method="get">
+    <input id="c" type="text" name="usrtel" placeholder="Phone Number">
+    <br>
+    <br>
+    <input id="b" type="submit" value="Save Phone Number">
+</form>
+<hr>
+<p id="a"> Profile Url </p>
+<p id="d" >Did you know you can change your PlenTree profile url? You can set a unique name, so people can easily search and find your profile.</p>
+<form id="p" action="#" method="get">
+    <input id="e"type="text" name="url" placeholder="www.plentree.com/profile/abc1">
+    <input id="b" type="submit" value="Save Profile URL">
+</form>
+<hr>
+<p id="a"> Account </p>
+<br>
+<form id="a" action="#" method="get"><p id="f">First Name:</p>
+    <input id="e" type="text" name="fname">
+</form>
+
+
+
+
+
+</div>
+
     
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
