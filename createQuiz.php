b1d48b5730759363e5bf6d128cc92fd2d2693fb0
AlishbaTasleem <41019089+AlishbaTasleem@users.noreply.github.com>
2018-07-30 20:18:47 +0500

    create quiz

--- createQuiz.php
+++ createQuiz.php
@@ -19,7 +19,35 @@
             <p class="formLabels">Quiz Time</p>
             <input class="textField text" type="text" name="qTime">
         </div>
-        
+    <div class="questionDiv">
+        <div class="quest" >
+            <p class="quizlabels">Question</p>
+            <input class="textField text" type="text" name="question">
+        </div>
+            <div class="quizOpt">
+            <p class="quizOptions">Option 1</p>
+            <input class="textField text" type="text" name="option1">
+        </div>
+        <div class="quizOpt">
+            <p class="quizOptions">Option 2</p>
+            <input class="textField text" type="text" name="option2">
+        </div>
+        <div class="quizOpt">
+        <p class="quizOptions">Option 3</p>
+            <input class="textField text" type="text" name="option3">
+        </div>
+        <div class="quizOpt">
+        <p class="quizOptions">Option 4</p>
+            <input class="textField text" type="text" name="option4">
+        </div>
+        <div class="quest" >
+            <p class="quizlabels">Correct Answer</p>
+            <input class="textField text" type="text" name="correctAnswer">
+        </div>
+        <i class="fas fa-plus-circle"></i>
+    </div>
+    <input id="submitQuest" class="submitBtn" type="submit" value="Save Changes">
+
     </form>
 
 </div>
